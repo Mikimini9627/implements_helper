028b126d67a8e3efbd1c0dc55bfe1f34cf165d59
yawatamikiya <yawatamikiya>
2025-02-02 14:20:54 +0900

    初回コミット

---
+++ README.rst
@@ -0,0 +1,15 @@
+=====================
+implements_helper
+=====================
+
+--------
+使用方法
+--------
+
+.. code-block:: python
+
+    from implements_helper import *
+
+    # 準備中...
+..
+
---
+++ deploy.bat
@@ -0,0 +1,8 @@
+rmdir /s /q build
+rmdir /s /q dist
+rmdir /s /q implements_helper.egg-info
+rmdir /s /q implements_helper\__pycache__
+
+python setup.py sdist
+python setup.py bdist_wheel
+twine upload --repository testpypi dist/*
